585f0bf6b646d32b880687addf5612c60a45b97e
Stefan Griffin <griffinstef39@gmail.com>
2021-05-20 09:53:00 -0400

    Beginning Form class

--- README.md
+++ README.md
@@ -4,10 +4,14 @@
 
 [1 - Project setup ](../../tree/588ef4ed6b903d5ee2e75997618e47ab77316e9e/)
 
-[2 - forms hard coded ](../../tree/855b0ec1cdf2141159707a53e5101546336b6d2d/)
+[2 - Forms hard-coded ](../../tree/855b0ec1cdf2141159707a53e5101546336b6d2d/)
 
-[3 - Next ](../../tree//)
+[3 - Added static files ](../../tree/28972ac7fe1dbb9cf83dadf6b943d8a841fc574f/)
 
 [4 - Next ](../../tree//)
+
+[5 - Next ](../../tree//)
+
+[5 - Next ](../../tree//)
 
 [5 - Next ](../../tree//)
--- reviews/views.py
+++ reviews/views.py
@@ -3,22 +3,34 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def review(request):
-        if request.method == 'POST':
-                entered_username = request.POST['username']
 
-                if entered_username == "" and len(entered_username) > 10:
-                        return render(request, "reviews/review.html", {
-                                "has_error": True
-                        })
-                print(entered_username)
-                return HttpResponseRedirect("/thank-you")
+
+
+
+
+
+
+
+def review(request):
+    if request.method == 'POST':
+        entered_username = request.POST['username']
+
+        if entered_username == "":
+            return render(request, "reviews/review.html", {
+                "has_error": True
+                    })
+        print(entered_username)
+        return HttpResponseRedirect("/thank-you")
                         
 
-        return render(request, "reviews/review.html", {
+    return render(request, "reviews/review.html", {
                 "has_error": False
         })
 
 
 def thank_you(request):
         return render(request, "reviews/thank_you.html")
+            
+
+
+          
