08f05a38804fc746ca979cad0d5f1640e5563f72
Stefan Griffin <griffinstef39@gmail.com>
2021-06-02 21:38:07 -0400

    Changed function view with class view

--- reviews/views.py
+++ reviews/views.py
@@ -1,36 +1,28 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .forms import ReviewForm
+from django.views import View
+
+
 
 # Create your views here.
 
-from .forms import ReviewForm
-from .models import Review
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
 
-def review(request):
-    if request.method == 'POST':
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
+
+    def post(self, request):
         form = ReviewForm(request.POST)
 
         if form.is_valid():
-            # print(form.cleaned_data)
-            review = Review(
-                user_name=form.cleaned_data['user_name'], 
-                review_text=form.cleaned_data['review_text'], 
-                rating=form.cleaned_data['rating']
-                )
-            review.save()
+            form.save()
             return HttpResponseRedirect("/thank-you")
-
-    else:
-        form = ReviewForm()
-                        
-    return render(request, "reviews/review.html", {
-            "form": form
-        })
 
 
 def thank_you(request):
         return render(request, "reviews/thank_you.html")
             
-
-
-          
